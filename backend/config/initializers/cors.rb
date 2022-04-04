a92a397d9af9fc28eb7d2b06ffba71d8aed5ee91
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-04-04 12:19:18 +0900

    API通信設定

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,4 +7,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
+
+    # 本番環境用のオリジン設定
+    allow do
+        origins ENV["FRONT_BASEURL"].to_s
+
+        resource '*',
+            headers: :any,
+            methods: [:get, :post, :put, :patch, :delete, :options, :head],
+            credentials: true
+    end
 end
