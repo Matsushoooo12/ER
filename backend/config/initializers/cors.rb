6e9814fc0f0ffb91d5037379f4021352e237ab8a
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-04-04 12:29:50 +0900

    HTTP通信設定

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,15 +1,5 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      # Front(React)側は3000ポートで繋ぐのでoriginsは3000を許可します
-      origins 'localhost:3000'
-  
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
-
-    # 本番環境用のオリジン設定
-    allow do
         origins ENV["FRONT_BASEURL"].to_s
 
         resource '*',
